65053d5dc63ce4ebc1da1c9bddd653eeae8f3dd9
graxia0322 <graxia@umich.edu>
2026-01-28 21:01:58 -0500

    extended legend lines for visibility

--- scripts/03_visualization.r
+++ scripts/03_visualization.r
@@ -772,7 +772,10 @@ plot_cdf_by_device <- function(
       values = pressure_linetypes,
       name = "Device Pressure Drop",
       labels = str_replace(names(pressure_linetypes), "_", " "),
-      guide = guide_legend(override.aes = list(linewidth = 1.5))
+      guide = guide_legend(
+        override.aes = list(linewidth = 1.2),
+        keywidth = unit(3, "cm")
+      )
     ) +
     labs(
       x = "Particle Size (µm)",
@@ -918,7 +921,10 @@ plot_cdf_by_pressure <- function(
       values = device_linetypes,
       name = "Device Resistance",
       labels = str_to_title(str_replace_all(names(device_linetypes), "_", " ")),
-      guide = guide_legend(override.aes = list(linewidth = 1.5))
+      guide = guide_legend(
+        override.aes = list(linewidth = 1.2),
+        keywidth = unit(3, "cm")
+      )
     ) +
     labs(
       x = "Particle Size (µm)",
